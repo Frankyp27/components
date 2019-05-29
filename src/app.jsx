ba66bd3c584166a4a633e597e01ec0510cdfce94
Frankyp27 <frankyp27@gmail.com>
2019-05-29 15:14:16 +0300

    react

--- src/app.jsx
+++ src/app.jsx
@@ -1,53 +1,158 @@
 const contentNode = document.getElementById('contents');
-//const continents = ['Africa','America','Asia','Australia','Europe'];
-//const message = continents.map(c => `Hello ${c}!`).join(' ');
-//const component = <p>{message}</p>;      // A simple JSX component
-//ReactDOM.render(component, contentNode);  // Render the component inside the content Node
+// //const continents = ['Africa','America','Asia','Australia','Europe'];
+// //const message = continents.map(c => `Hello ${c}!`).join(' ');
+// //const component = <p>{message}</p>;      // A simple JSX component
+// //ReactDOM.render(component, contentNode);  // Render the component inside the content Node
 
-// class IssueList extends React.Component {
-//   render() {
-//     return (
-//       <div>This is a placeholder for the issue list.</div>
-//     );
-//   }
+// // class IssueList extends React.Component {
+// //   render() {
+// //     return (
+// //       <div>This is a placeholder for the issue list.</div>
+// //     );
+// //   }
+// // }
+// // ReactDOM.render(<IssueList />, contentNode); // Render the component inside the content Node
+
+
+// class IssueFilter extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for the Issue Filter.</div>
+//         )
+//     }
 // }
-// ReactDOM.render(<IssueList />, contentNode); // Render the component inside the content Node
+// class IssueTable extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for a table of Issues.</div>
+//         )
+//     }
+// }
+// class IssueAdd extends React.Component {
+   // render() {
+      //return (
+       // <div>This is a placeholder for an Issue Add entry form.</div>
+    //  )
+  // } 
+// class IssueLists extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Issue Tracker</h1>
+//                 <IssueFilter />
+//                 <hr />
+//                 <IssueTable />
+//                 <hr />
+//                 <IssueAdd />
+//             </div>
+//         );
+//     }
+// }
 
-
-class IssueFilter extends React.Component {
+class IssueRow extends React.Component {
     render() {
+      const issue = this.props.issue;
       return (
-        <div>This is a placeholder for the Issue Filter.</div>
-      )
-    }
-  }
-  class IssueTable extends React.Component {
-    render() {
-      return (
-        <div>This is a placeholder for a table of Issues.</div>
-      )
-    }
-  }
-  class IssueAdd extends React.Component {
-    render() {
-      return (
-        <div>This is a placeholder for an Issue Add entry form.</div>
-      )
-    }
-  }
-  class IssueLists extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Issue Tracker</h1>
-          <IssueFilter />
-          <hr />
-          <IssueTable />
-          <hr />
-          <IssueAdd />
-        </div>
+        <tr>
+          <td>{issue.id}</td>
+          <td>{issue.status}</td>
+          <td>{issue.owner}</td>
+          <td>{issue.created.toDateString()}</td>
+          <td>{issue.effort}</td>
+          <td>
+            {issue.completionDate ? issue.completionDate.toDateString() : ""}
+          </td>
+          <td>{issue.title}</td>
+        </tr>
       );
     }
   }
-  ReactDOM.render(<IssueLists />, contentNode); // Render the component inside the content Node
+   //IssueRow.propTypes = {
+   // issue_id: React.PropTypes.number.isRequired,
+   // issue_title: React.PropTypes.string
+//};
+class IssueFilter extends React.Component {
+    render() {
+      return <div>This is a placeholder for the Issue Filter.</div>;
+    }
+  }
   
+class IssueTable extends React.Component {
+    render() {
+        const issueRows = this.props.issues.map(issue => (
+          <IssueRow key={issue.id} issue={issue} />
+        ));
+        return (
+          <table className="bordered-table">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Status</th>
+                <th>Owner</th>
+                <th>Created</th>
+                <th>Effort</th>
+                <th>Completion Date</th>
+                <th>Title</th>
+              </tr>
+            </thead>
+            <tbody>{issueRows}</tbody>
+          </table>
+        );
+      }
+    }
+      class IssueAdd extends React.Component {
+        render() {
+          return <div>This is a placeholder for the Issue Add entry form.</div>;
+        }
+      }
+      
+      const issues = [
+        {
+          id: 1,
+          status: "Open",
+          owner: "Ravan",
+          created: new Date("2016-08-15"),
+          effort: 5,
+          completionDate: undefined,
+          title: "Error in console when clicking Add"
+        },
+        {
+          id: 2,
+          status: "Assigned",
+          owner: "Eddie",
+          created: new Date("2016-08-16"),
+          effort: 14,
+          completionDate: new Date("2016-08-30"),
+          title: "Missing bottom border on panel"
+        }
+      ];
+      
+      class IssueList extends React.Component {
+        render() {
+          return (
+            <div>
+              <h1>Issue Tracker</h1>
+              <IssueFilter />
+              <hr />
+              <IssueTable issues={issues} />
+              <hr />
+              <IssueAdd />
+            </div>
+          );
+        }
+      }      
+
+class BorderWrap extends React.Component{
+    render(){
+        const borderedStyle={border: "1px solid silver",padding: 6};
+        return(
+          <div style={borderedStyle}>
+              {this.props.children}
+          </div>
+        );
+    }
+}
+
+ReactDOM.render(<IssueList/>, contentNode); // Render the component inside the content Node
+
+
--- static/app.js
+++ static/app.js
@@ -1,62 +1,225 @@
 const contentNode = document.getElementById('contents');
-//const continents = ['Africa','America','Asia','Australia','Europe'];
-//const message = continents.map(c => `Hello ${c}!`).join(' ');
-//const component = <p>{message}</p>;      // A simple JSX component
-//ReactDOM.render(component, contentNode);  // Render the component inside the content Node
+// //const continents = ['Africa','America','Asia','Australia','Europe'];
+// //const message = continents.map(c => `Hello ${c}!`).join(' ');
+// //const component = <p>{message}</p>;      // A simple JSX component
+// //ReactDOM.render(component, contentNode);  // Render the component inside the content Node
 
-// class IssueList extends React.Component {
-//   render() {
-//     return (
-//       <div>This is a placeholder for the issue list.</div>
-//     );
-//   }
+// // class IssueList extends React.Component {
+// //   render() {
+// //     return (
+// //       <div>This is a placeholder for the issue list.</div>
+// //     );
+// //   }
+// // }
+// // ReactDOM.render(<IssueList />, contentNode); // Render the component inside the content Node
+
+
+// class IssueFilter extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for the Issue Filter.</div>
+//         )
+//     }
 // }
-// ReactDOM.render(<IssueList />, contentNode); // Render the component inside the content Node
+// class IssueTable extends React.Component {
+//     render() {
+//         return (
+//             <div>This is a placeholder for a table of Issues.</div>
+//         )
+//     }
+// }
+// class IssueAdd extends React.Component {
+// render() {
+//return (
+// <div>This is a placeholder for an Issue Add entry form.</div>
+//  )
+// } 
+// class IssueLists extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Issue Tracker</h1>
+//                 <IssueFilter />
+//                 <hr />
+//                 <IssueTable />
+//                 <hr />
+//                 <IssueAdd />
+//             </div>
+//         );
+//     }
+// }
 
-
-class IssueFilter extends React.Component {
+class IssueRow extends React.Component {
   render() {
+    const issue = this.props.issue;
     return React.createElement(
-      'div',
+      "tr",
       null,
-      'This is a placeholder for the Issue Filter.'
+      React.createElement(
+        "td",
+        null,
+        issue.id
+      ),
+      React.createElement(
+        "td",
+        null,
+        issue.status
+      ),
+      React.createElement(
+        "td",
+        null,
+        issue.owner
+      ),
+      React.createElement(
+        "td",
+        null,
+        issue.created.toDateString()
+      ),
+      React.createElement(
+        "td",
+        null,
+        issue.effort
+      ),
+      React.createElement(
+        "td",
+        null,
+        issue.completionDate ? issue.completionDate.toDateString() : ""
+      ),
+      React.createElement(
+        "td",
+        null,
+        issue.title
+      )
     );
   }
 }
-class IssueTable extends React.Component {
+//IssueRow.propTypes = {
+// issue_id: React.PropTypes.number.isRequired,
+// issue_title: React.PropTypes.string
+//};
+class IssueFilter extends React.Component {
   render() {
     return React.createElement(
-      'div',
+      "div",
       null,
-      'This is a placeholder for a table of Issues.'
+      "This is a placeholder for the Issue Filter."
+    );
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    const issueRows = this.props.issues.map(issue => React.createElement(IssueRow, { key: issue.id, issue: issue }));
+    return React.createElement(
+      "table",
+      { className: "bordered-table" },
+      React.createElement(
+        "thead",
+        null,
+        React.createElement(
+          "tr",
+          null,
+          React.createElement(
+            "th",
+            null,
+            "Id"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Status"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Owner"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Created"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Effort"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Completion Date"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Title"
+          )
+        )
+      ),
+      React.createElement(
+        "tbody",
+        null,
+        issueRows
+      )
     );
   }
 }
 class IssueAdd extends React.Component {
   render() {
     return React.createElement(
-      'div',
+      "div",
       null,
-      'This is a placeholder for an Issue Add entry form.'
+      "This is a placeholder for the Issue Add entry form."
     );
   }
 }
-class IssueLists extends React.Component {
+
+const issues = [{
+  id: 1,
+  status: "Open",
+  owner: "Ravan",
+  created: new Date("2016-08-15"),
+  effort: 5,
+  completionDate: undefined,
+  title: "Error in console when clicking Add"
+}, {
+  id: 2,
+  status: "Assigned",
+  owner: "Eddie",
+  created: new Date("2016-08-16"),
+  effort: 14,
+  completionDate: new Date("2016-08-30"),
+  title: "Missing bottom border on panel"
+}];
+
+class IssueList extends React.Component {
   render() {
     return React.createElement(
-      'div',
+      "div",
       null,
       React.createElement(
-        'h1',
+        "h1",
         null,
-        'Issue Tracker'
+        "Issue Tracker"
       ),
       React.createElement(IssueFilter, null),
-      React.createElement('hr', null),
-      React.createElement(IssueTable, null),
-      React.createElement('hr', null),
+      React.createElement("hr", null),
+      React.createElement(IssueTable, { issues: issues }),
+      React.createElement("hr", null),
       React.createElement(IssueAdd, null)
     );
   }
 }
-ReactDOM.render(React.createElement(IssueLists, null), contentNode); // Render the component inside the content Node
+
+class BorderWrap extends React.Component {
+  render() {
+    const borderedStyle = { border: "1px solid silver", padding: 6 };
+    return React.createElement(
+      "div",
+      { style: borderedStyle },
+      this.props.children
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(IssueList, null), contentNode); // Render the component inside the content Node
